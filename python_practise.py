b57c828232ae768b29f6b77fdf65257ed819bce3
Mohanakri <mohanakrishna109@gmail.com>
2025-08-06 19:13:10 +0530

    updated

--- python_practise.py
+++ python_practise.py
@@ -1,13 +1,14 @@
-def k(s):
-     
- 
+keys = ['app', 'script', 'program']
+vals = [1, 3, 5]
 
+# Method 1: Loop
+D2 = {}
+for (k, v) in zip(keys, vals): 
+    D2[k] = v
 
-def pali(s):
-     return True if s==s[::-1] else False
+# Method 2: dict constructor (preferred)
+D3 = dict(zip(keys, vals))
 
-
-     
-vari='mk'
-print(k(vari))
-print(pali(vari))
+# Method 3: Dictionary comprehension
+D4 = {k: v for (k, v) in zip(keys, vals)}
+print(D2,D3,D4,sep='\n')
